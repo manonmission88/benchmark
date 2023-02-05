6e5590d64342ab6bccd8f6dd16b668be48fbff83
manonmission88 <manishnewray88@gmail.com>
2023-02-05 13:11:25 -0500

    benchmarks initial phase

---
+++ first_benchmark.py
@@ -0,0 +1,4 @@
+'''32-bit Integer operation benchmark (reference time: 100 seconds)
+1010 additions (of integer constants)
+5 × 109 multiplication (of integer constants)
+2 × 109 division (of integer constants) '''
