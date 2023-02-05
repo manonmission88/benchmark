2a1a1150c0fdfd059136eedaf9d74d43b8d682c8
manonmission88 <manishnewray88@gmail.com>
2023-02-05 15:06:30 -0500

    added the loop for division started from 1 to avoid zero division errro

--- first_benchmark.py
+++ first_benchmark.py
@@ -13,6 +13,13 @@ for i in range(10**10):
 #for multiplictaion 
 for i in range(5*10**9):
     initial_value *= i 
+#for division
+for i in range(1,2*10**9):
+    initial_value /= i
 
+end_time = time.time()
+total_time_taken = end_time-start_time
+
+print('total time taken to execute  ',total_time_taken)
 
 
