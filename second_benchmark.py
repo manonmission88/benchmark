12dc586e445a27e4854d380e2f2b6930344a4961
manonmission88 <manishnewray88@gmail.com>
2023-02-05 16:20:17 -0500

    added loop for divison

--- second_benchmark.py
+++ second_benchmark.py
@@ -1,8 +1,8 @@
 '''64-bit Floating point operation benchmark (reference time: 100 seconds)
 Same as integer, use double precision floating point numbers instead of integer.
---•10^10 additions (of integer constants)
-•5 × 10^9 multiplication (of integer constants)
-•2 × 10^9 division (of integer constants)'''
+--•10^10 additions 
+•5 × 10^9 multiplication 
+•2 × 10^9 division '''
 import time 
 
 start_time = time.time()
@@ -13,4 +13,13 @@ for i in range(10**10):
 #for multiplication 
 for i in range(5*10**9):
     initial_value *= i
+# for division
+for i in range(1,2*10**9): # started from 1 to avoid zero division error 
+    initial_value /= i
+
+end_time= time.time()
+total_time_taken = end_time -start_time
+
+print('total time taken is ',total_time_taken)
+
 
